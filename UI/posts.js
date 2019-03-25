1677e9cd4bd6bbc1a03df29b0709ec704490c496
Vika <lazarchuk.viktoriya@gmail.com>
2019-03-25 17:04:58 +0300

    classes added

---
+++ UI/posts.js
@@ -0,0 +1,72 @@
+var photoPosts = [
+  {
+    id: "1", //user
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2019-02-23T23:40:00'),
+    author: 'User',
+    photoLink: "pictures/1.jpg"
+  },
+  {
+    id: '2',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2019-03-23T18:10:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/2.jpg"
+  },
+  {
+    id: '3', //user
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-03-21T17:40:00'),
+    author: 'User',
+    photoLink: "pictures/3.jpg"
+  },
+  {
+    id: '4', //user
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-02-26T16:19:00'),
+    author: 'User',
+    photoLink: "pictures/4.jpg"
+  },
+  {
+    id: '5',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-03-18T06:55:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/5.jpg"
+  },
+  {
+    id: '6',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-01-23T08:40:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/6.jpg"
+  },
+  {
+    id: '7',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-01-31T16:35:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/7.jpg"
+  },
+  {
+    id: '8',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-02-23T15:56:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/8.jpg"
+  },
+  {
+    id: '9',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-03-05T19:40:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/9.jpg"
+  },
+  {
+    id: '10',
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod aliqua. Ud exercitation ex ea commodo consequat.",
+    createdAt: new Date('2018-01-01T05:40:00'),
+    author: 'Viktoriyaa',
+    photoLink: "pictures/10.jpg"
+  },
+  ];
