504786cc7fe4ed932e5d93a6081a604c88427bc6
Vika <lazarchuk.viktoriya@gmail.com>
2019-05-06 16:33:33 +0300

    Kinda database

---
+++ sql/photos.sql
@@ -0,0 +1,2 @@
+insert into photo_post (description, creation_date, photo_link, user_id) values ("Sed sed dolor eget nisi pulvinar porttitor condimentum vitae ex.", "19.04.30/06:00:11", "images/10.jpg", 3);
+commit;
